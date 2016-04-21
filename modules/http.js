1df74825913b46ad6d943137e7371d203bf325d6
gabemarshall <gabemarshall@me.com>
2016-04-21 10:15:58 -0400

    Removed dead code, improved concurrency

--- modules/http.js
+++ modules/http.js
@@ -3,7 +3,7 @@ var request = require('request'),
     clc = require('cli-color'),
     error = clc.red.bold,
     warn = clc.yellow,
-    notice = clc.blue;
+    notice = clc.cyanBright;
 
 var dirp = request.defaults({pool: {maxSockets: 50},'proxy':''})
 
@@ -19,38 +19,6 @@ exports.cookies = function(cookies) {
     }
 
 }
-
-function tryAgain(payload, string){
-    dirp({ url: payload, followAllRedirects: false, headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36', 'Cookie': cooks } }, function(error, response, body) {
-        try {
-            if (response.statusCode != 404) {
-                if (response.statusCode === 200 || response.statusCode === 301 || response.statusCode === 302 ) {
-                    if (payload.match(/fZPfRfWKLfaLkfz/)) {
-                        if (!string) {
-                            console.log("A %s response code is being returned for a file that shouldn't exist", response.statusCode);
-                            console.log("Use --string to provide a pattern to match for 'Page does not exist'");
-                            process.exit();
-                        } else {
-                            console.log("Response string matched for a file that shouldn't exist");
-                        }
-                    } else {
-                        console.log(notice("[+] "+payload+" looks to be valid"))
-                    }
-                    if (body.match(/Log In/)) {
-                        //console.log("Response string matched (Page Not Found or Invalid Session)");
-                    }
-                }
-            } else {
-
-            }
-            count++;
-        } catch (err) {
-           console.log(err);
-        }
-
-    });
-}
-
 
 function clean(payload) {
     var s = payload;
@@ -80,7 +48,7 @@ exports.get = function(url, path, next, string) {
                             console.log("Response string matched for a file that shouldn't exist");
                         }
                     } else {
-                        console.log("%s looks to be valid", payload)
+                        console.log(notice("[+] "+payload+" looks to be valid"))
                     }
                     if (body.match(test)) {
                         console.log("Response string matched (Page Not Found or Invalid Session)");
